55b9e01e1ef7d5ecd65bf4ea46d356ef89fb386b
yanxin-star <yanxin-2007@qq.com>
2026-03-02 18:15:49 +0800

    加上了下位机的launch执行代码

---
+++ src/launch_pack/launch/arm_driver_calc_rviz2.launch.py
@@ -0,0 +1,41 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    urdf_path = os.path.join(
+        get_package_share_directory("arm"),
+        "model", "arm.urdf"
+    )
+    # 读取URDF内容
+    with open(urdf_path, 'r') as inf:
+        robot_desc = inf.read()
+
+    robot_state_pub = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_desc}]
+    )
+
+    arm_calc = Node(
+        package="arm_calc",
+        executable="arm_calc"
+    )
+
+    arm_drive=Node(
+        package="arm_drive",
+        executable="arm_drive"
+    )
+
+    rviz2_config_path=os.path.join(
+        get_package_share_directory("launch_pack"),
+        "rviz", "display_config.rviz"
+    )
+
+    rviz2 = Node(
+        package="rviz2",
+        executable="rviz2",
+        arguments=["-d", rviz2_config_path]  # 可选，指定rviz配置文件
+    )
+    return LaunchDescription([arm_drive,robot_state_pub, arm_calc, rviz2])
